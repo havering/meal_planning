7d6bae360a6bdabb1ec7a571f4e6bbe1851ac4af
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2017-02-23 16:14:32 -0800

    Bootstrap, styling, users have dedicated recipe page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :recipes]
 
   # GET /users
   # GET /users.json
@@ -59,6 +59,10 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def recipes
+    @user_recipes = @user.recipes
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   resources :ingredients, only: [:show, :edit, :update, :destroy]
 
-  resources :users
+  get 'users/:id/recipes' => 'users#recipes', as: 'user_recipes'
 
+  resources :users
   root 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
