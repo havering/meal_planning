7a888391ed72b73deb64e588675d4c5c4930a194
Diana O'Haver <c4t3rp1ll4r@gmail.com>
2017-04-20 13:19:02 -0700

    User can set their name from Devise registration now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :meals
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :recipes do
     resources :ingredients, only: [:index, :new, :create]
   end
